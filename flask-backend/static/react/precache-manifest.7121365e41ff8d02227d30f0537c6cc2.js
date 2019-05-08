fcc0ccfcd3da30931174ad75a35cc6f89758e105
Anna B <Anna.Bespalov@student.htw-berlin.de>
2019-05-08 17:44:28 +0200

    Initial commit

---
+++ flask-backend/static/react/precache-manifest.7121365e41ff8d02227d30f0537c6cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859feb6c965901248b3abf25669ee4dc",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "a9175bef08a66cfff353",
+    "url": "/static/react/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/react/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a9175bef08a66cfff353",
+    "url": "/static/react/js/main.6eea9bf7.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
